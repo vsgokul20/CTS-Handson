3c391aee05ca6153404b7a246472001d64d3983b
Gokul Anandh <vsgokul20@gmail.com>
2021-06-27 19:37:29 +0530

    Delete Day 2/Session 1/1 directory

--- Day 2/Session 1/1/src/main/java/com/cognizant/springlearn/service/exception/CountryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.springlearn.service.exception;
-
-public class CountryNotFoundException extends Exception {
-	public CountryNotFoundException(String message) {
-		super(message);
-	}
-
-}
